b68baac5adf52db4d879ee066b96a1a8f03d20c8
Mehmet Barış Güdül <114955963+barisgudul@users.noreply.github.com>
2024-10-12 02:50:44 +0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,16 @@
+import requests
+
+parameters = {
+    "amount":20,
+    "category":27,
+    "type":"boolean",
+}
+
+response =requests.get(url="https://opentdb.com/api.php?amount=20&category=27&type=boolean",params=parameters)
+response.raise_for_status()
+data=response.json()
+
+
+
+question_data = data["results"]
+
